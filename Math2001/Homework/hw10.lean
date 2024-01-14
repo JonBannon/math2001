c463cbb09562301410b6288d8845f2de73c64e20
JonBannon <59937998+JonBannon@users.noreply.github.com>
2024-01-14 11:12:40 -0500

    Update hw10.lean
    Removed HTML link

--- Math2001/Homework/hw10.lean
+++ Math2001/Homework/hw10.lean
@@ -13,8 +13,7 @@ open Function
 
 /-! # Homework 10
 
-Don't forget to compare with the text version,
-https://hrmacbeth.github.io/math2001/Homework.html#homework-10
+Don't forget to compare with the text version
 for clearer statements and any special instructions. -/
 
 
