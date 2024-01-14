39f041c028fd8c51686cfda9d55f05fb2dc7c3b9
JonBannon <59937998+JonBannon@users.noreply.github.com>
2024-01-14 11:09:12 -0500

    Update hw0.lean
    Removed html comment

--- Math2001/Homework/hw0.lean
+++ Math2001/Homework/hw0.lean
@@ -5,12 +5,6 @@ import AutograderLib
 
 math2001_init
 
-/-! # Homework 0
-
-Don't forget to compare with the text version,
-https://hrmacbeth.github.io/math2001/Homework.html#homework-0
-for clearer statements and any special instructions. -/
-
 
 @[autograded 5]
 theorem problem1 {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 :=
